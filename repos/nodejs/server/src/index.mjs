3f646e3409e2acac4d73cd30401462b238c160a1
Jonathan Adami <pitilezard@gmail.com>
2021-05-21 08:09:41 +1000

    better than hello world isn't it?

--- repos/nodejs/server/src/index.mjs
+++ repos/nodejs/server/src/index.mjs
@@ -1,7 +1,9 @@
 import express from "express";
 
+import homepage from "./homepage.mjs";
+
 const app = express();
 
-app.use("/", (req, res) => res.send("Hello World!"));
+app.use("/", (req, res) => res.send(homepage));
 
 app.listen(4000);
