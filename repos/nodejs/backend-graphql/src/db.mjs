f04b12bc7e1bc5b52ab80819e163e4c168879cad
Jonathan Adami <pitilezard@gmail.com>
2021-05-22 19:17:44 +1000

    ability to switch the schemas when needed by having it configurable

--- repos/nodejs/backend-graphql/src/db.mjs
+++ repos/nodejs/backend-graphql/src/db.mjs
@@ -2,7 +2,8 @@ import sequelize from "sequelize";
 import initModels from "./models/index.mjs";
 const { Sequelize } = sequelize;
 
-const config = { dialect: "sqlite", storage: "/db/backend-graphql.sqlite3" };
+const storage = process.env.SCHEMA_CONFIG_DB_PATH || "/db/backend-graphql.sqlite3";
+const config = { dialect: "sqlite", storage };
 const db = new Sequelize(config);
 
 export const models = initModels(db);
