d87eee48bd0be27e3c21b9d4c2f1b5a866d88f8a
Jonathan Adami <pitilezard@gmail.com>
2021-05-22 16:09:01 +1000

    docker environment to control app behaviour

--- repos/nodejs/server/src/index.mjs
+++ repos/nodejs/server/src/index.mjs
@@ -4,7 +4,7 @@ import homepage from "./homepage.mjs";
 import backend from "@agql.js/backend-graphql";
 
 const app = express();
-app.debug = true;
+app.debug = process.env.DEBUG == 1;
 
 if (app.debug) {
     app.use((req, res, next) => {
@@ -13,13 +13,17 @@ if (app.debug) {
     });
 }
 
-backend(app);
+const mode = process.env.SERVER_MODE;
+if (mode == "backend" || mode == "all") {
+    backend(app);
+}
 
 app.use("/", (req, res) => res.send(homepage));
 
-app.listen(4000);
+const port = process.env.SERVER_PORT || 4000;
+app.listen(port);
+console.log(`Running on http://localhost:${port}`);
 
-console.log("Running on http://localhost:4000");
 if (app.debug) {
     console.log("Routes registered");
     console.log(app._router.stack.map((route, i) => `${route.name}: ${route.regexp}`));
