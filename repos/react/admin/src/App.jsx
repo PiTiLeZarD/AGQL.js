8bd03a9eb1a962d23a5533259ce2cd15f5f08932
Jonathan Adami <pitilezard@gmail.com>
2021-05-22 15:34:15 +1000

    Basic app layout

--- repos/react/admin/src/App.jsx
+++ repos/react/admin/src/App.jsx
@@ -1,8 +1,13 @@
-import { Fragment } from "react";
-import { Grid, Paper } from "@material-ui/core";
+import { Fragment, useState } from "react";
+import { Grid, List, ListItem, ListItemText, Card, CardContent, CardHeader, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 import AgqlEffect from "./AgqlEffect";
 
 const App = (props) => {
+    const [selectedEntity, setSelectedEntity] = useState(null);
+
     return (
         <Fragment>
             <style type="text/css">{`
@@ -23,11 +28,44 @@ const App = (props) => {
             <Grid container spacing={8}>
                 <Grid item xs={2} />
                 <Grid item xs={4}>
-                    <Paper style={{ padding: "1.5em" }}>Here entities</Paper>
+                    <Card>
+                        <CardHeader
+                            title="Entities"
+                            subheader="10 Entities currently in the database"
+                            action={
+                                <IconButton>
+                                    <AddIcon />
+                                </IconButton>
+                            }
+                        />
+                        <CardContent>
+                            <List>
+                                {new Array(10).fill().map((_, i) => (
+                                    <ListItem button onClick={(ev) => setSelectedEntity(i)}>
+                                        <ListItemText primary={`Entity ${i}`} />
+                                        <DoubleArrowIcon />
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </CardContent>
+                    </Card>
                 </Grid>
 
                 <Grid item xs={4}>
-                    <Paper style={{ padding: "1.5em" }}>here details about entities</Paper>
+                    {selectedEntity != null && (
+                        <Card>
+                            <CardHeader
+                                title={`Entity ${selectedEntity}`}
+                                subheader={`ID: ${selectedEntity} Created At: September 14, 2016`}
+                                action={
+                                    <IconButton>
+                                        <MoreVertIcon />
+                                    </IconButton>
+                                }
+                            />
+                            <CardContent>Here fields and links</CardContent>
+                        </Card>
+                    )}
                 </Grid>
                 <Grid item xs={2} />
             </Grid>
