2f1080d9a801b276883c94ab144ac54de5a6b509
Jonathan Adami <pitilezard@gmail.com>
2021-05-24 15:58:36 +1000

    default value for sqlite schema db should be a non docker compatible value

--- repos/nodejs/backend-graphql/src/db.mjs
+++ repos/nodejs/backend-graphql/src/db.mjs
@@ -2,7 +2,7 @@ import sequelize from "sequelize";
 import initModels from "./models/index.mjs";
 const { Sequelize } = sequelize;
 
-const storage = process.env.SCHEMA_CONFIG_DB_PATH || "/db/backend-graphql.sqlite3";
+const storage = process.env.SCHEMA_CONFIG_DB_PATH || "./backend-graphql.sqlite3";
 const config = { dialect: "sqlite", storage };
 const db = new Sequelize(config);
 
